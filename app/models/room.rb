f53459014f799fffadaf29fde3263c1d855d7e89
Zengo-Master <8435740@gmail.com>
2020-08-14 15:04:37 +0900

    チャットルームの削除機能の実装

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,7 +1,9 @@
 class Room < ApplicationRecord
   has_many :room_users
-  has_many :users, through: :room_users
-  has_many :messages
+  # roomが削除されるとuserも削除される
+  has_many :users, through: :room_users, dependent: :destroy
+  # roomが削除れるとmessageも削除される
+  has_many :messages, dependent: :destroy
 
   validates :name, presence: true
 end
