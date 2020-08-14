03abfa9e4c9f0ec01424f7db2aad0d346c9082a3
Zengo-Master <8435740@gmail.com>
2020-08-14 14:18:35 +0900

    メッセージ送信機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,27 @@
 class MessagesController < ApplicationController
+
   def index
+    @message = Message.new
+    @room = Room.find(params[:room_id])
   end
+
+  def create
+    @room = Room.find(params[:room_id])
+    @message = @room.messages.new(message_params)
+    # 保存できたらメッセージのページへ
+    if @message.save
+      redirect_to room_messages_path(@room)
+    # 保存できなければ、indexアクションで同じページへ
+    else
+      render :index
+    end
+  end
+
+  private
+
+  # チャットルームに紐づいたメッセージのみを保存
+  def message_params
+    params.require(:message).permit(:content).merge(user_id: current_user.id)
+  end
+
 end
