a6f5da7c5eb8b5a17ebb01bc2688f5e62fe5788d
Zengo-Master <8435740@gmail.com>
2020-08-14 15:47:16 +0900

    Active Storageの導入

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -22,7 +22,7 @@ class MessagesController < ApplicationController
 
   # チャットルームに紐づいたメッセージのみを保存
   def message_params
-    params.require(:message).permit(:content).merge(user_id: current_user.id)
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
   end
 
 end
